29e599887f77ee688b072fcd28bbd1fa72ba6cde
top <top@MB14>
2021-03-06 17:50:09 +0100

    adjust some pathes to linux.
    VTM developing only works with linux

--- src/main/java/org/oscim/test/MapsforgeS3DBTest.java
+++ src/main/java/org/oscim/test/MapsforgeS3DBTest.java
@@ -21,7 +21,7 @@ import java.io.File;
 public class MapsforgeS3DBTest extends MapsforgeTest {
 	//public static final String mapFile = "C:\\OfflineMaps\\mapfiles\\download.mapsforge.org\\switzerland_V5.map";
 	//public static final String mapFile = "C:\\OfflineMaps\\mapfiles\\www.openandromaps.org\\Switzerland_ML.map";
-   public static final String mapFile = "D:\\OfflineMaps\\mapfiles\\mf\\niedersachsen_V5.map";
+   public static final String mapFile = "/home/top/oruxmaps/mapfiles/niedersachsenV5.map";
    //public static final String mapFile = "D:\\OfflineMaps\\mapfiles\\mf\\france.map";
 
 
